22195828b66e4d914e25300ad9dc6789034fab87
Signalzero <chlehdnjs@gmail.com>
2022-06-18 15:59:16 +0900

    add login UI

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import styles from './header.module.css';
 
-const Header = ({ onLogout }) => {
+const Header = ({ onLogout }) => (
   <header className={styles.header}>
     {onLogout && (
       <button className={styles.logout} onClick={onLogout}>
         Logout
       </button>
     )}
-    <img className={styles.logo} src='/imgages/logo.png' alt='logo' />
+    <img className={styles.logo} src='/images/logo.png' alt='logo' />
     <h1 className={styles.title}>Business Card Maker</h1>
-  </header>;
-};
+  </header>
+);
 
 export default Header;
